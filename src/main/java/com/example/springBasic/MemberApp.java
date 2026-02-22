d9c6bf3a9014d45b3c274de46526b8585faa34e4
nsnoo <nsnoo9905@gmail.com>
2026-02-22 21:42:11 +0900

    test

--- src/main/java/com/example/springBasic/MemberApp.java
+++ src/main/java/com/example/springBasic/MemberApp.java
@@ -8,7 +8,7 @@ public class MemberApp {
 
     public static void main(String[] args) {
         //AppConfig appConfig = new AppConfig();
-        //MemberService memberService = appConfig.memberService(); test
+        //MemberService memberService = appConfig.memberService(); test11111
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
