c117a20e9d9e4b3699ecb20ffa3013828afa7873
nsnoo <nsnoo9905@gmail.com>
2026-03-02 17:41:42 +0900

    스프링 컨테이너와 스프링 빈

---
+++ src/test/java/com/example/springBasic/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,38 @@
+package com.example.springBasic.beanfind;
+
+import com.example.springBasic.AppConfig;
+import com.example.springBasic.member.MemberService;
+import com.example.springBasic.member.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 X")
+    void findBeanByNameX() {
+        assertThrows(NoSuchBeanDefinitionException.class,
+                ()-> ac.getBean("xxxxx", MemberService.class));
+    }
+}
